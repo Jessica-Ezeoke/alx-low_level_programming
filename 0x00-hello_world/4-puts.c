9639bf9864b3c33a02f69ce25a113d1484558521
Jessica-Ezeoke <jessicaewesi@gmail.com>
2023-08-12 05:43:25 -0700

    An executive file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-/** main - The execution of the file begins from the main function
-*
-* Return: This returns zero(0)
-*/
 
-int main(void)
-{
-     puts("\"Programming is like building a multilingual puzzle);
-     return (0);  
-}	
+
+
+
+
+
+
+
+
+
+
