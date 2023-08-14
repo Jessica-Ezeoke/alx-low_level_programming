3b89bf52092507aad75fce1b3d736f6b643d212f
Jessica-Ezeoke <jessicaewesi@gmail.com>
2023-08-14 02:47:07 -0700

    Pay attention to betty syles

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-/** main - The execution of the file begins from the main function
-*
-* Return: This returns zero(0)
-*/
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
 
 int main(void)
 {
-     puts("\"Programming is like building a multilingual puzzle);
-     return (0);  
-}	
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
